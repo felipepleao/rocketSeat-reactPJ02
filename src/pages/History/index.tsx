e965c406f6d4d7a0a1fb93fb0577ee2db4884fbd
Felipe Leão <felipeleao.tech@gmail.com>
2024-04-17 23:36:20 -0300

    Pagina Home finalizada

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,7 @@
+import { HistoryContainer } from "./styles";
+
+export function History() {
+  return <HistoryContainer>
+    <h1>Ola mundo</h1>
+  </HistoryContainer>;
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,21 +1,20 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-
   }
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+    box-shadow: 0 0 0 2px ${(props) => props.theme["green-500"]};
   }
 
   body {
-    background-color: ${(props) => props.theme['gray-900']};
-    color: ${(props) => props.theme['gray-300']};
+    background-color: ${(props) => props.theme["gray-900"]};
+    color: ${(props) => props.theme["gray-300"]};
   }
 
   body, input, textarea, button {
@@ -24,4 +23,4 @@ export const GlobalStyle = createGlobalStyle`
     font-size: 1rem;
   }
 
-`
+`;
