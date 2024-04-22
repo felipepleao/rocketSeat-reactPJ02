15919e8d86d704712dc8b70552e2cf3805e50501
Felipe Leão <felipeleao.tech@gmail.com>
2024-04-22 08:44:23 -0300

    Adicionando página History

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,56 @@
-import { HistoryContainer } from "./styles";
+import { HistoryContainer, HistoryList } from "./styles";
 
 export function History() {
-  return <HistoryContainer>
-    <h1>Ola mundo</h1>
-  </HistoryContainer>;
+  return (
+    <HistoryContainer>
+      <h1>Ola mundo</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
